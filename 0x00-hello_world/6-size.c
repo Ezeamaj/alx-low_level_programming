1cf5d4f29e95bd0d6ae631363b1837e4447d49b7
Ezeama Irechukwu <ubarechi@gmail.com>
2023-09-07 16:34:05 -0700

    Hello world C scripts 0-8

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int L;
+	float f;
+
+	printf("size of a char is: %lu. \n", (unsigned long) size of(c));
+	printf("size of a int is: %lu. \n", (unsigned long) size of(i));
+	printf("size of a long int is: %lu. \n", (unsigned long) size of(l));
+	printf("size of a long long int is: %lu. \n", (unsigned long) size of(L));
+	printf("size of a float is: %lu. \n", (unsigned long) size of(f));
+
+	return (0)
+}
