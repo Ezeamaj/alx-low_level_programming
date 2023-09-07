5e98e136aac75e4e3341f8d950d7cb45f46e1590
Ezeama Irechukwu <ubarechi@gmail.com>
2023-09-07 16:50:30 -0700

    Adjustment to scripts 1-6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,11 @@
 
 int main(void)
 {
-	char c;
-	int i;
-	long int l;
-	long long int L;
-	float f;
-
-	printf("size of a char is: %lu. \n", (unsigned long) size of(c));
-	printf("size of a int is: %lu. \n", (unsigned long) size of(i));
-	printf("size of a long int is: %lu. \n", (unsigned long) size of(l));
-	printf("size of a long long int is: %lu. \n", (unsigned long) size of(L));
-	printf("size of a float is: %lu. \n", (unsigned long) size of(f));
-
-	return (0)
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
+
